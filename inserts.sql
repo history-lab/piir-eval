9686074d8e3cef2a3e80575f70f78e546337eb9b
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-01-10 13:37:21 -0500

    inital commit

---
+++ inserts.sql
@@ -0,0 +1,30 @@
+-- add methods
+insert into piir_eval.methods(method_code, description) 
+values ('capone', 'Capital One Data Profiler'),
+       ('muckrock', 'Common Rexeg library used in the documentcloud add on');
+
+-- add entities
+insert into piir_eval.entities(entity_code, description) 
+values ('ban', 'Bank account number, 10-18 digits'),
+       ('credit_card', 'Credit card bumber'),
+       ('drvers_license', 'Drivers license'),
+       ('ssn','Social security number');
+       -- ('address','address'),
+       -- ('person','person''s name'),
+       -- ('email_address','Email address'),
+       -- ('phone_number','Phone number'),
+       -- ('url','URL'),
+       -- ('uuid','UUID'),
+       -- ('hash_or_key','md5, sha1, sha256, random hash, etc.'),
+       -- ('ipv4','IP address, version 4'),
+       -- ('ipv6','IP address, version 6'),
+       -- ('mac_address','MAC address'),
+
+-- first pass for tests, add all the cases from cables
+insert into piir_eval.tests (doc_id, corpus)
+select doc_id, 'cfpf'
+   from foiarchive.docs 
+   where corpus = 'cfpf' and 
+   body ~ '(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}';
+
+
