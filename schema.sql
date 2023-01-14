f02a4277e0f8393770ac278cf14ce149cc092c4f
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-01-13 19:13:32 -0500

    Added table testruns

--- schema.sql
+++ schema.sql
@@ -18,6 +18,12 @@ create table if not exists piir_eval.runs
     (run_id      int generated always as identity primary key,
      method_code text not null references piir_eval.methods,
      start_time  timestamp with time zone not null default current_timestamp);
+create table if not exists piir_eval.testruns
+    (testrun_id  int generated always as identity primary key,
+     run_id      int references piir_eval.runs,
+     test_id     int references piir_eval.tests,
+     start_time  timestamp with time zone not null default current_timestamp,
+     unique(run_id, test_id));
 create table if not exists piir_eval.results 
     (result_id   int generated always as identity primary key,
      test_id     int  not null references piir_eval.tests,
