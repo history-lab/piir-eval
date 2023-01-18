e5042cfd7896e59e5ec9b2788fd3344915b147b6
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-01-18 13:19:21 -0500

    Added doc related attributes to results_view

--- schema.sql
+++ schema.sql
@@ -31,12 +31,16 @@ create table if not exists piir_eval.results
      entity_text text not null,
      start_idx   int,
      end_idx     int);
-create or replace view piir_eval.results_view as
-select r.method_code, r.run_id, tr.start_time, tr.test_id, tr.testrun_id,
+create view piir_eval.results_view as
+select r.method_code, r.run_id, tr.start_time, tr.test_id, 
+       tr.testrun_id, t.doc_id, t.corpus,
+       'http://history-lab.org/documents/' || t.doc_id docurl,
        res.result_id, res.entity_code, res.entity_text, 
        res.start_idx, res.end_idx
    from piir_eval.runs r join piir_eval.testruns tr 
                               on (r.run_id = tr.run_id)
+                         join piir_eval.tests t
+                              on (tr.test_id = t.test_id)
                          left join piir_eval.results res
                               on (tr.testrun_id = res.testrun_id)
    where r.run_id = (select max(run_id)
