84017678c870fd32706c6839f0108e70be71cc96
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-01-18 13:51:13 -0500

    Added entity_pair column to results_view

--- schema.sql
+++ schema.sql
@@ -36,6 +36,7 @@ select r.method_code, r.run_id, tr.start_time, tr.test_id,
        tr.testrun_id, t.doc_id, t.corpus,
        'http://history-lab.org/documents/' || t.doc_id docurl,
        res.result_id, res.entity_code, res.entity_text, 
+       '(' || entity_code || ', ' || entity_text || ')' entity_pair,
        res.start_idx, res.end_idx
    from piir_eval.runs r join piir_eval.testruns tr 
                               on (r.run_id = tr.run_id)
