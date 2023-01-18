5bc5f9fd72a25ef4d32a8bb03025c880367fdd76
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-01-18 13:04:13 -0500

    Initial commit of results_view

--- schema.sql
+++ schema.sql
@@ -31,3 +31,14 @@ create table if not exists piir_eval.results
      entity_text text not null,
      start_idx   int,
      end_idx     int);
+create or replace view piir_eval.results_view as
+select r.method_code, r.run_id, tr.start_time, tr.test_id, tr.testrun_id,
+       res.result_id, res.entity_code, res.entity_text, 
+       res.start_idx, res.end_idx
+   from piir_eval.runs r join piir_eval.testruns tr 
+                              on (r.run_id = tr.run_id)
+                         left join piir_eval.results res
+                              on (tr.testrun_id = res.testrun_id)
+   where r.run_id = (select max(run_id)
+                      from piir_eval.runs
+                      where method_code = r.method_code);
