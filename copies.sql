61f562f372c7587bea08502f456f8993a7ca7a0e
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-03-14 18:12:11 -0400

    Added copies of ground_truth_view and comparison

--- copies.sql
+++ copies.sql
@@ -1,2 +1,4 @@
 \copy (select * from piir_eval.cm_compare) to 'cm_compare.csv' with format csv header 
 \copy (select * from piir_eval.results_view) to 'results.csv' csv header 
+\copy (select * from piir_eval.ground_truth_view) to 'gt.csv' csv header
+\copy (select * from piir_eval.comparisons) to 'comparisons.csv' csv header 
