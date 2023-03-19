619d275a4872aa30eb6794a53f1988094a6536d0
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-03-19 09:48:15 -0400

    Direct output to results subdirectory

--- copies.sql
+++ copies.sql
@@ -1,4 +1,4 @@
-\copy (select * from piir_eval.cm_compare) to 'cm_compare.csv' with format csv header 
-\copy (select * from piir_eval.results_view) to 'results.csv' csv header 
-\copy (select * from piir_eval.ground_truth_view) to 'gt.csv' csv header
-\copy (select * from piir_eval.comparisons) to 'comparisons.csv' csv header 
+\copy (select * from piir_eval.cm_compare) to 'results/cm_compare.csv' with format csv header 
+\copy (select * from piir_eval.results_view) to 'results/results.csv' csv header 
+\copy (select * from piir_eval.ground_truth_view) to 'results/gt.csv' csv header
+\copy (select * from piir_eval.comparisons) to 'results/comparisons.csv' csv header 
