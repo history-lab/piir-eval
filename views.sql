4e9e2b96cdf72dc251b3735f7ad8861befa53430
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-03-19 15:35:21 -0400

    Updated results_view

--- views.sql
+++ views.sql
@@ -1,21 +1,19 @@
 create view piir_eval.tasks_view as
-select t.task_id, t.taskset_id, ts.name taskset_name, t.corpus
-       t.doc_id, 
-   from piir_eval.tasks t join piir_eval.taskset ts
+select t.task_id, t.taskset_id, ts.name taskset_name, t.corpus,
+       t.doc_id, dt.canonical_url doc_url
+   from piir_eval.tasks t join piir_eval.tasksets ts
                               on (t.taskset_id = ts.taskset_id)
                           join covid19.dcml_training dt
-                              on (t.doc_id = dt.item_id)
+                              on (t.doc_id = dt.item_id::text);
 
 create view piir_eval.results_view as
 select r.method_code, r.run_id, tr.start_time, tr.task_id, 
-       tr.taskrun_id, t.doc_id, t.corpus,
-       'http://history-lab.org/documents/' || t.doc_id docurl,
+       tr.taskrun_id, t.doc_id, t.corpus, t.doc_url,
        res.result_id, res.entity_code, res.entity_text, 
-       '(' || entity_code || ', ' || entity_text || ')' entity_pair,
        res.start_idx, res.end_idx
    from piir_eval.runs r join piir_eval.taskruns tr 
                               on (r.run_id = tr.run_id)
-                         join piir_eval.tasks t
+                         join piir_eval.tasks_view t
                               on (tr.task_id = t.task_id)
                          left join piir_eval.results res
                               on (tr.taskrun_id = res.taskrun_id)
