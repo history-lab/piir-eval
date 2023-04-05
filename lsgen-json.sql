168425aa964ccc9dd9ab7bfb1c03cd3b063d55a8
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-04-05 17:16:10 -0400

    Switch from results_view to ls_dcml_data; fixed format

--- lsgen-json.sql
+++ lsgen-json.sql
@@ -1,30 +1,24 @@
-select jsonb_agg(
-        jsonb_build_object('id', t.task_id,  
-                           'data', jsonb_build_object('ref_id', t.doc_id,
-                                                      'text', t.body),
-                           'predictions', json_build_object('model_version', 'models/combindednlp',
-                                                            'results', (select jsonb_agg(
-        jsonb_build_object('from_name', 'label',
-                           'to_name', 'text',
-                           'type', 'labels',
-                           'value', json_build_object('start', r.start_idx,
-                                                      'end', r.end_idx,
-                                                      'text', r.entity_text,
-                                                      'labels', json_build_array(r.entity_code)
-                                                     )
-                          )
-                ) 
-   from piir_eval.results_view r
-   where r.task_id = t.task_id and r.method_code = 'muckrock'
-))
-                          )
-                )
-    from piir_eval.tasks t
-    where exists (select 1 
-                    from piir_eval.results_view 
-                    where task_id = t.task_id and
-                          start_idx is not null)
-    group by t.task_id
-    order by t.task_id
-    limit 3;
-    
+-- "data": {"url": "s3://history-lab-labeled-data/pii-cfpf-redaction/source/1977STATE161504.txt"}
+select jsonb_pretty(jsonb_agg(jsonb_build_object(
+      'id', t.task_id, 
+      'data', jsonb_build_object(
+            'ref_id', t.doc_id,
+            'url', 'https://history-lab-labeled-data.s3.amazonaws.com/pii-dcml-redaction/source/' || 
+                    t.doc_id || '.txt'),
+      'predictions', jsonb_build_array(jsonb_build_object(
+            'model_version', 'models/combindednlp',
+            'result', (
+                  select coalesce(jsonb_agg(jsonb_build_object(
+                  'from_name', 'label',
+                  'to_name', 'text',
+                  'type', 'labels',
+                  'value', jsonb_build_object(
+                        'start', r.start_idx,
+                        'end', r.end_idx,
+                        'text', r.entity_text,
+                        'labels', json_build_array(r.entity_code)))),
+                  jsonb_build_array()) 
+                  from piir_eval.tool_detected_pii r 
+                  where r.task_id = t.task_id))))))
+from piir_eval.tasks t join piir_eval.ls_dcml_data d 
+      on (t.task_id = d.task_id);
