9ddf440b17f17f611e44ea3d4acefb9f21d9f320
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-04-28 01:28:09 -0400

    End to end version

--- intercoder-table.sql
+++ intercoder-table.sql
@@ -24,6 +24,8 @@ create table piir_eval.intercoder (
     doc_url              text,
     primary key (task_id, start_idx)
 );
+
+--
 insert into piir_eval.intercoder (task_id, start_idx,
     jules_start_idx, jules_end_idx, jules_entity_code, jules_entity_text, 
     jules_completed, jules_id, doc_id, doc_url) 
@@ -38,52 +40,66 @@ where completed = (select max(completed)
                                   mj.end_idx = j.end_idx);
 
 update piir_eval.intercoder i
-   set (jules_start_idx, jules_end_idx, jules_entity_code, jules_entity_text, 
-    jules_completed, jules_id),
-   
-   
-   (tool, t_entity_code, t_entity_text, t_start_idx, t_end_idx) =
-       (select tool, entity_code, entity_text, start_idx, end_idx
-           from piir_eval.tool_detected_pii t
-           where t.task_id = c.task_id and
-                 abs(t.start_idx - c.start_idx) <= 1);
+   set (isobel_start_idx, isobel_end_idx, isobel_entity_code, isobel_entity_text, 
+        isobel_completed, isobel_id)=
+       (select start_idx, end_idx, entity_code, entity_text, 
+               completed, ground_truth_id
+            from piir_eval.isobel_ground_truth gt
+            where i.task_id = gt.task_id and
+                  abs(i.start_idx - gt.start_idx) <= 1 and
+                  gt.completed = (select max(completed)
+                                    from piir_eval.isobel_ground_truth mgt
+                                    where mgt.task_id = gt.task_id and
+                                          mgt.start_idx = gt.start_idx and
+                                          mgt.end_idx = gt.end_idx))
+ ;
 
-insert into comparisons(task_id, start_idx, tool, 
-    t_entity_code, t_entity_text, t_start_idx, t_end_idx, 
-    doc_id, doc_url)
-select task_id, start_idx, tool, entity_code, entity_text, 
-       start_idx, end_idx, doc_id, doc_url
-    from piir_eval.tool_detected_pii t
-    where not exists (select 1
-                        from piir_eval.comparisons c
-                        where c.task_id = t.task_id and 
-                              c.t_start_idx = t.start_idx) and
-          exists (select 1
-                    from piir_eval.comparisons c 
-                    where c.task_id = t.task_id) and 
-          end_idx = (select max(end_idx)
-                        from piir_eval.tool_detected_pii d
-                        where d.task_id = t.task_id    and 
-                              d.start_idx = t.start_idx);
+insert into piir_eval.intercoder (task_id, start_idx,
+    isobel_start_idx, isobel_end_idx, isobel_entity_code, isobel_entity_text, 
+    isobel_completed, isobel_id, doc_id, doc_url) 
+select task_id, start_idx,
+    start_idx, end_idx, entity_code, entity_text, 
+    completed, ground_truth_id,doc_id, doc_url
+from piir_eval.isobel_ground_truth gt
+where gt.completed = (select max(completed)
+                            from piir_eval.isobel_ground_truth mgt
+                            where gt.task_id = mgt.task_id and
+                                  gt.start_idx = mgt.start_idx and
+                                  gt.end_idx = mgt.end_idx) and
+      not exists (select 1 from piir_eval.intercoder i
+                     where i.task_id = gt.task_id and
+                           i.start_idx = gt.start_idx);
 
-update comparisons
-    set redaction_result = case when gt_start_idx is not null and 
-                                      t_start_idx is not null then 'TP'
-                                when gt_start_idx is null and
-                                      t_start_idx is not null then 'FP'
-                                when gt_start_idx is not null and
-                                      t_start_idx is null     then 'FN'
-                           end,
-        entity_result = case when gt_entity_code = t_entity_code 
-                                                              then 'TP'
-                             when gt_entity_code != t_entity_code or
-                                  gt_entity_code is null      then 'FP'                                  
-                             when gt_entity_code is not null and 
-                                  t_entity_code  is null      then 'FN'  
-                        end;
 
-\copy (select * from comparisons order by doc_id, start_idx) to 'comparisons.csv' csv header
+update piir_eval.intercoder i
+   set (natalie_start_idx, natalie_end_idx, natalie_entity_code, natalie_entity_text, 
+        natalie_completed, natalie_id)=
+       (select start_idx, end_idx, entity_code, entity_text, 
+               completed, ground_truth_id
+            from piir_eval.natalie_ground_truth gt
+            where i.task_id = gt.task_id and
+                  abs(i.start_idx - gt.start_idx) <= 1 and
+                  gt.completed = (select max(completed)
+                                    from piir_eval.natalie_ground_truth mgt
+                                    where mgt.task_id = gt.task_id and
+                                          mgt.start_idx = gt.start_idx and
+                                          mgt.end_idx = gt.end_idx))
+ ;
 
-\i scores-table.sql
-\i scores-calc.sql
-\copy (select * from scores order by score_id) to 'scores.csv' csv header
+insert into piir_eval.intercoder (task_id, start_idx,
+    natalie_start_idx, natalie_end_idx, natalie_entity_code, natalie_entity_text, 
+    natalie_completed, natalie_id, doc_id, doc_url) 
+select task_id, start_idx,
+    start_idx, end_idx, entity_code, entity_text, 
+    completed, ground_truth_id,doc_id, doc_url
+from piir_eval.natalie_ground_truth gt
+where gt.completed = (select max(completed)
+                            from piir_eval.natalie_ground_truth mgt
+                            where gt.task_id = mgt.task_id and
+                                  gt.start_idx = mgt.start_idx and
+                                  gt.end_idx = mgt.end_idx) and
+      not exists (select 1 from piir_eval.intercoder i
+                     where i.task_id = gt.task_id and
+                           i.start_idx = gt.start_idx);
+
+\copy (select * from intercoder order by task_id) to 'intercoder.csv' csv header
